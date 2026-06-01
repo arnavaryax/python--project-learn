5d04ddaf96ae1c4342f080edd3e19b809909d198
Arnav Arya <arnavarya.nx@gmail.com>
2026-06-01 15:21:48 +0530

    beginner Python projects
    ### 1. Smart Movie Tracker
    A Python program that:
    - Asks the user to enter 3 favorite movies
    - Stores them in a list
    - Asks for 1 recently watched movie
    - Adds the new movie using `append()`
    - Sorts the list alphabetically using `sort()`
    - Prints the updated sorted list
    
    **Concepts used:**
    - `input()`
    - Lists
    - `append()`
    - `sort()`
    - Printing output
    
    **How to run:**
    ```bash
    python movie_tracker.py
    ```

---
+++ movie_tracker.py
@@ -0,0 +1,9 @@
+# mov1 = input("Enter your fav movie 1:")
+# mov2 = input("Enter your fav movie 2:")
+# mov3 = input("Enter your fav movie 3:")
+# movielist = [mov1, mov2, mov3]
+# movierecent = input("input one more movie that they recently watched:")
+# movielist.append(movierecent)
+# movielist.sort()
+# print (movielist, "updated, sorted list of movies")
+print("Bill:"/n"yoo")
